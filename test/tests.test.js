a8b85aacdbb12b62f476bc9e319dc48bd63c8870
tclindner <tclindner@users.noreply.github.com>
2020-09-05 11:48:57 -0500

    Create tests.test.js

---
+++ test/tests.test.js
@@ -0,0 +1,33 @@
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const eslint = require('eslint');
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const isPlainObj = require('is-plain-obj');
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const eslintConfig = require('../index.js');
+
+describe('eslint config tests', () => {
+  describe('eslint object', () => {
+    test('should be an object', () => {
+      expect(isPlainObj(eslintConfig)).toBe(true);
+    });
+  });
+
+  describe('run eslint and make sure it runs', () => {
+    test('eslint should run without failing', () => {
+      const code = `function myString(myString: String): String {return myString;}`;
+      const expectedErrorLineNum = 1;
+      const expectedErrorColumnNum = 1;
+      const linter = new eslint.CLIEngine({
+        useEslintrc: false,
+        configFile: '.eslintrc.json',
+      });
+      const errors = linter.executeOnText(code).results[0].messages;
+      const error = errors[0];
+
+      expect(error.ruleId).toStrictEqual('func-style');
+      expect(error.line).toStrictEqual(expectedErrorLineNum);
+      expect(error.column).toStrictEqual(expectedErrorColumnNum);
+      expect(error.message).toStrictEqual('Expected a function expression.');
+    });
+  });
+});
