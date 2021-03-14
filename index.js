2b26255cddd5870b79bc176935787934be9ba081
tclindner <tclindner@users.noreply.github.com>
2021-03-14 14:48:11 -0500

    Add array-type rule
    Closes #2

--- index.js
+++ index.js
@@ -14,4 +14,11 @@ module.exports = {
     'import/extensions': ['.js', '.mjs', '.jsx', '.ts', '.tsx', '.d.ts'],
     'import/external-module-folders': ['node_modules', 'node_modules/@types'],
   },
+  rules: {
+    // https://github.com/typescript-eslint/typescript-eslint/blob/v4.0.1/packages/eslint-plugin/docs/rules/array-type.md
+    '@typescript-eslint/array-type': ['error', {
+      default: 'array'
+    }],
+
+  },
 };
