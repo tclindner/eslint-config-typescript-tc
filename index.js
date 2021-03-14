4e251df250bdd2dabfdce773e05ed6d4752f3545
tclindner <tclindner@users.noreply.github.com>
2021-03-14 14:45:43 -0500

    Settings updates for file parsing and extension resolution

--- index.js
+++ index.js
@@ -2,4 +2,16 @@ module.exports = {
   extends: ['plugin:@typescript-eslint/recommended', 'prettier/@typescript-eslint'],
   parser: '@typescript-eslint/parser',
   plugins: ['@typescript-eslint', 'prettier'],
+  settings: {
+    'import/parsers': {
+      '@typescript-eslint/parser': ['.ts', '.tsx', '.d.ts'],
+    },
+    'import/resolver': {
+      node: {
+        extensions: ['.mjs', '.js', '.json', '.ts', '.d.ts'],
+      },
+    },
+    'import/extensions': ['.js', '.mjs', '.jsx', '.ts', '.tsx', '.d.ts'],
+    'import/external-module-folders': ['node_modules', 'node_modules/@types'],
+  },
 };
