61487fb0c96d5ae551a2da18a9a6d8764ecd421f
Thomas Lindner <tclindner@users.noreply.github.com>
2021-12-04 00:21:28 -0600

    ESLint v8 support (#246)
    * Upgrade eslint and typescript eslint
    
    * Add temporary Jest resolver
    
    * Update tests for new ESLint export

--- test/tests.test.js
+++ test/tests.test.js
@@ -1,16 +1,14 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
-const eslint = require('eslint');
+const {ESLint} = require('eslint');
 const isPlainObj = require('is-plain-obj');
 const eslintConfig = require('../index');
 /* eslint-enable @typescript-eslint/no-var-requires */
 
-const lintFile = (files) => {
-  const linter = new eslint.CLIEngine({
-    useEslintrc: false,
-    configFile: '.eslintrc.json',
-  });
-  const errors = linter.executeOnFiles(files).results[0].messages;
-  const error = errors[0];
+const lintFile = async (files) => {
+  const linter = new ESLint();
+  const results = await linter.lintFiles(files);
+  const errorMessages = results[0].messages;
+  const error = errorMessages[0];
 
   return error;
 };
@@ -23,11 +21,11 @@ describe('eslint config tests', () => {
   });
 
   describe('run eslint and make sure it runs', () => {
-    test('eslint should run without failing', () => {
+    test('eslint should run without failing', async () => {
       const file = './test/fixtures/invalid-func-style.ts';
       const expectedErrorLineNum = 1;
       const expectedErrorColumnNum = 1;
-      const error = lintFile(file);
+      const error = await lintFile(file);
 
       expect(error.ruleId).toStrictEqual('func-style');
       expect(error.line).toStrictEqual(expectedErrorLineNum);
@@ -37,11 +35,11 @@ describe('eslint config tests', () => {
   });
 
   describe('enum format', () => {
-    test('eslint should run without failing', () => {
+    test('eslint should run without failing', async () => {
       const file = './test/fixtures/invalid-enum.ts';
       const expectedErrorLineNum = 1;
       const expectedErrorColumnNum = 6;
-      const error = lintFile(file);
+      const error = await lintFile(file);
 
       expect(error.ruleId).toStrictEqual('@typescript-eslint/naming-convention');
       expect(error.line).toStrictEqual(expectedErrorLineNum);
@@ -53,11 +51,11 @@ describe('eslint config tests', () => {
   });
 
   describe('enum member format', () => {
-    test('eslint should run without failing', () => {
+    test('eslint should run without failing', async () => {
       const file = './test/fixtures/invalid-enum-member.ts';
       const expectedErrorLineNum = 2;
       const expectedErrorColumnNum = 3;
-      const error = lintFile(file);
+      const error = await lintFile(file);
 
       expect(error.ruleId).toStrictEqual('@typescript-eslint/naming-convention');
       expect(error.line).toStrictEqual(expectedErrorLineNum);
